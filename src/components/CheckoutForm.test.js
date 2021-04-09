a512ef3d2a21fb854caa5a4293c73d942ebdcfea
Austin Greer <agreer.ag12@gmail.com>
2021-04-09 15:54:18 -0500

    first test is passing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
+
+    const formHeader = screen.queryByText(/checkout form/i)
+
+    expect(formHeader).toBeInTheDocument()
+    expect(formHeader).toBeTruthy()
+    expect(formHeader).toHaveTextContent(/checkout form/i)
+});
 
 test("form shows success message on submit with form details", () => {});
